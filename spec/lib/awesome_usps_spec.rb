840ac26b1df519c677c9cc54a50eab68b51f7362
David Ryder <david@libryder.com>
2012-07-21 21:36:29 -0700

    added settings.rb

--- spec/lib/awesome_usps_spec.rb
+++ spec/lib/awesome_usps_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
-USPS_USERNAME = '782G50004335'
-
 describe AwesomeUsps do
+
+  it "settings.rb exists" do
+    File.exist?('spec/settings.rb').should be_true
+  end
 
   it "can be instantiated" do
     AwesomeUsps::USPS.new(USPS_USERNAME).should_not be_nil
